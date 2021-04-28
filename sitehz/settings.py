2bf270cde9271e716195a3cd0d8bbc32a696c7f2
Your Name <you@example.com>
2021-04-28 13:49:41 -0500

    urls pages

--- sitehz/settings.py
+++ sitehz/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #MY_APPS
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'sitehz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
